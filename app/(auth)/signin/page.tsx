1694871f6dc7a8360d7378234ed15f6b355871ae
Tyler Davis <tdavis@evolutionmfg.co>
2023-11-30 10:28:07 -0800

    further work to contact us page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,75 +1,115 @@
+import Image from 'next/image'
+import React from 'react';
+import FeatImage01 from '@/public/images/TimHeadshot.png'
+import FeatImage02 from '@/public/images/RonHeadshot.png'
+import FeatImage03 from '@/public/images/JeffHeadshot.png'
+import ReactLogo from '@/public/images/address-book-regular.svg'
+import ReactLogo2 from '@/public/images/phone-solid.svg'
+
 export const metadata = {
-  title: 'Sign In - Open PRO',
-  description: 'Page description',
+  title: 'Contact Us - Evolution MFG.',
+  description: 'Meet the team',
 }
 
 import Link from 'next/link'
 
 export default function SignIn() {
   return (
-    <section className="relative">
+    <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        <div className="py-12 md:py-20 border-t border-gray-800">
 
-          {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1">Welcome back. We exist to make entrepreneurship easier.</h1>
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h2 mb-4">Meet the team</h1>
+            <p className="text-xl text-gray-400"></p>
           </div>
 
-          {/* Form */}
-          <div className="max-w-sm mx-auto">
-            <form>
-              <div className="flex flex-wrap -mx-3">
-                <div className="w-full px-3">
-                  <button className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center">
-                    <svg className="w-4 h-4 fill-current text-white opacity-75 shrink-0 mx-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" />
-                    </svg>
-                    <span className="h-6 flex items-center border-r border-white border-opacity-25 mr-4" aria-hidden="true"></span>
-                    <span className="flex-auto pl-16 pr-8 -ml-16">Sign in with Google</span>
-                  </button>
+          {/* Items */}
+          <div className="grid gap-20">
+
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage01} width={300} height={305} alt="Features 01" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Owner</div>
+                  <h3 className="h3 mb-3">Tim Netter</h3>
+                  <p className="text-lg text-gray-400 mb-4">Previously, Production Manager that has helped in the design and building of equipment primarily for the semiconductor industry, but have also worked with design and building of medical and industrial equipment. I have done everything from piece parts to automated equipment. It is a great feeling to build something from start to finish and then see that your customer is satisfied with the end result.</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo} width={20} height={20} alt="Features 02" />
+                      <span>tnetter@evolutionmfg.co</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo2} width={20} height={20} alt="Features 02" />
+                      <span>971-806-6560</span>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </form>
-            <div className="flex items-center my-6">
-              <div className="border-t border-gray-700 border-dotted grow mr-3" aria-hidden="true"></div>
-              <div className="text-gray-400">Or, sign in with your email</div>
-              <div className="border-t border-gray-700 border-dotted grow ml-3" aria-hidden="true"></div>
-            </div>
-            <form>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                  <input id="email" type="email" className="form-input w-full text-gray-300" placeholder="you@yourcompany.com" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">Password</label>
-                  <input id="password" type="password" className="form-input w-full text-gray-300" placeholder="Password (at least 10 characters)" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="flex items-center">
-                      <input type="checkbox" className="form-checkbox" />
-                      <span className="text-gray-400 ml-2">Keep me signed in</span>
-                    </label>
-                    <Link href="/reset-password" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">Forgot Password?</Link>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-purple-600 hover:bg-purple-700 w-full">Sign in</button>
-                </div>
-              </div>
-            </form>
-            <div className="text-gray-400 text-center mt-6">
-              Don’t you have an account? <Link href="/signup" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">Sign up</Link>
             </div>
           </div>
+          <br />
+          <hr></hr>
+          <br />
+          {/* 2nd item */}
+          <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+            {/* Image */}
+            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
+              <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage02} width={300} height={305} alt="Features 02" />
+            </div>
+            {/* Content */}
+            <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+              <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                <div className="font-architects-daughter text-xl text-purple-600 mb-2">Sales Manager</div>
+                <h3 className="h3 mb-3">Ron Davis</h3>
+                <p className="text-lg text-gray-400 mb-4">A seasoned Sales Manager specializing in semiconductor sales, seamlessly integrating technical expertise with a passion for success. With a proven track record in navigating the complex landscape of semiconductor solutions, client relations, and strategic business development, John is committed to driving results and exceeding sales targets. Beyond the realm of silicon and circuits, he finds serenity by the water, casting lines and strategizing not only in the boardroom but also in the tranquility of his favorite fishing spots. A devoted follower of European football, Ron understands the precision and strategy required in the semiconductor industry, reflecting the same passion in his approach to the game. For a unique blend of semiconductor proficiency, strategic salesmanship, and shared excitement for fishing and football.</p>
+                <ul className="text-lg text-gray-400 -mb-2">
+                  <li className="flex items-center mb-2">
+                    <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo} width={20} height={20} alt="Features 02" />
+                    <span>rdavis@evolutionmfg.co</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo2} width={20} height={20} alt="Features 02" />
+                    <span>360-949-3116</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <br />
+          <hr></hr>
+          <br />
+          {/* 3rd item */}
+          <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
+                <Image className="max-w-full mx-auto md:max-w-none h-auto" src={FeatImage03} width={300} height={305} alt="Features 03" />
+              </div>
+              {/* Content */}
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Outside Sales Representative</div>
+                  <h3 className="h3 mb-3">Jeff Davis</h3>
+                  <p className="text-lg text-gray-400 mb-4">Greetings, I am Jeff Davis, an Outside Sales Representative who has recently relocated to the West Coast. With a background in cultivating valuable client relationships and a proficiency in identifying business opportunities, I am committed to enhancing your experience with our esteemed products and services. As I navigate the diverse markets of the West Coast, I also appreciate the scenic golf courses this region offers. Golf serves as both a pastime and a pursuit that aligns with the meticulous strategy required in sales. I invite the opportunity to connect with you—whether discussing sales strategies or engaging in a round of golf. Your consideration is greatly appreciated, and I look forward to contributing to the success of our collaborations.</p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                  <li className="flex items-center mb-2">
+                    <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo} width={20} height={20} alt="Features 02" />
+                    <span>jdavis@evolutionmfg.co</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <Image className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" src={ReactLogo2} width={20} height={20} alt="Features 02" />
+                    <span>717-951-5140</span>
+                  </li>
+                </ul>
+                </div>
+              </div>
+            </div>
 
         </div>
       </div>
