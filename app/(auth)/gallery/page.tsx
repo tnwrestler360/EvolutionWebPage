311e7d09aedc709b250323d8e84f397a501197c9
Tyler Davis <tdavis@evolutionmfg.co>
2023-12-08 14:28:07 -0800

    Code cleanup. Photo gallery generated using for loop.

--- app/(auth)/gallery/page.tsx
+++ app/(auth)/gallery/page.tsx
@@ -3,55 +3,10 @@
 import React from 'react';
 import ImageGallery from "react-image-gallery";
 import FeatImage01 from '@/public/images/TimImage.png'
-
-
-// const images = [
-//     {
-//       original: 'https://imgur.com/knjQL93.png',
-//       thumbnail: "https://i.imgur.com/lhNlmUV.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/nOzHgpx.png',
-//       thumbnail: "https://i.imgur.com/nIvTA7l.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/wtsjFlC.png',
-//       thumbnail: "https://i.imgur.com/YSda0LN.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/HoPhlew.png',
-//       thumbnail: "https://i.imgur.com/Al7t797.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/5DbkbN1.png',
-//       thumbnail: "https://i.imgur.com/HHmhzs3.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/lGE2up3.png',
-//       thumbnail: "https://i.imgur.com/8K6MLNn.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/7Tdph6v.png',
-//       thumbnail: "https://i.imgur.com/Ty0lBVd.png",
-//     },
-//     {
-//       original: 'https://i.imgur.com/7Tdph6v.png', //option 8
-//       thumbnail: "https://i.imgur.com/Ty0lBVd.png",
-//     },
-// ]
-
 const originalBase  = 'https://github.com/tnwrestler360/EvolutionWebPage/blob/master/public/images/original/'
 const thumbnailBase = 'https://raw.githubusercontent.com/tnwrestler360/EvolutionWebPage/master/public/images/thumbnail/'
+
 const images:any = []
-
-
-// import Link from 'next/link'
-// class MyGallery extends React.Component {
-//   render() {
-//     for (var i =  0; i<27; i++)
-//     return <ImageGallery items={images} />;
-//   }
-// }
 
 class MyGallery extends React.Component {
   render() {
@@ -61,7 +16,7 @@ class MyGallery extends React.Component {
           thumbnail: `${thumbnailBase}original${i}T.png?raw=true`,
         })
     }
-    return <ImageGallery items={images} />;
+    return <ImageGallery items={images} showPlayButton = {false}/>;
   }
 }
 
@@ -77,9 +32,10 @@ export default function Gallery() {
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
-
+        <h1 className="h2 mb-4 photo-gh">Photo Gallery</h1>
+            <hr></hr>
+            <br />
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h2 mb-4">Photo Gallery</h1>
             <p className="text-xl text-gray-400"></p>
             <MyGallery />
           </div>
--- components/hero.tsx
+++ components/hero.tsx
@@ -36,12 +36,6 @@ export default function Hero() {
             <br />
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">At Evolution Manufacturing LLC, we are committed to providing high-quality manufacturing services that exceed our clients' expectations. Our mission is to deliver innovative solutions that help our clients succeed.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
-              {/* <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-              </div> */}
             </div>
           </div>
 
@@ -54,18 +48,7 @@ export default function Hero() {
                 alt="Hero"
                 priority
               />
-              
-            {/* <svg className="w-16 h-16 sm:w-20 sm:h-20 hover:opacity-75 transition duration-150 ease-in-out" viewBox="0 0 88 88" xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <linearGradient x1="78.169%" y1="9.507%" x2="24.434%" y2="90.469%" id="a">
-                  <stop stopColor="#EBF1F5" stopOpacity=".8" offset="0%" />
-                  <stop stopColor="#EBF1F5" offset="100%" />
-                </linearGradient>
-              </defs>
-              <circle fill="url(#a)" cx="44" cy="44" r="44" />
-              <path className="fill-current text-purple-600" d="M52 44a.999.999 0 00-.427-.82l-10-7A1 1 0 0040 37V51a.999.999 0 001.573.82l10-7A.995.995 0 0052 44V44c0 .001 0 .001 0 0z" />
-            </svg> */}
-              
+
             </div>
             
             {/* Modal */}
